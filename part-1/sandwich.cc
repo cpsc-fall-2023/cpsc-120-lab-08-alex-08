a573e46b00f18002dbfe3e18e96a5e8d5846f610
Alex Acosta <AlexAcosta@csu.fullerton.edu>
2023-11-02 19:38:40 -0700

    Built code to determine what sandwhich the user inputs.

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Alex Acosta
+// alexacosta@csu.fullerton.edu
+// @AlexAcostaaa
+// Partners: @alone
 
 #include <iostream>
 #include <string>
@@ -7,17 +10,21 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
+  if (arguments.size() != 4) {
+    std::cout << "error: you must supply three arguments";
+    return 1;
+  }
 
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
-
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
-
+  std::string command{arguments.at(0)};
+  std::string protein{arguments.at(1)};
+  std::string bread{arguments.at(2)};
+  std::string condiment{arguments.at(3)};
+  std::cout << "./sandwich; " << command << "\n";
+  std::cout << "protein; " << protein << "\n";
+  std::cout << "bread; " << bread << "\n";
+  std::cout << "condiment; " << condiment << "\n";
+  std::cout << "Your order:\n"
+            << " A " << protein << " sandwich on " << bread << " with "
+            << condiment << ". ";
   return 0;
 }
